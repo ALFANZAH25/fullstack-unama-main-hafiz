1dcc0aded0c3586f18aa2a872da59a99d11532df
ALFANZAH25 <hafizalfanzah111@gmail.com>
2026-06-06 17:38:34 +0700

    materi 7 context (menambah fitur dark mode)

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -9,10 +9,18 @@ import { Counter, Profile } from "./components/3-state";
 import { Posts } from "./components/4-side-effect";
 import { CountRef, InputFocus } from "./components/5-referensi";
 import { TodoList } from "./components/6-reducer";
+import { useContext } from "react";
+import { ThemeContext, ThemeToggle } from "./components/7-context";
 
 const App = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div style={{ height: "2000dvh" }}>
+    <div style={{
+        height: "2000dvh",
+        backgroundColor: theme === "dark" ? "#222" : "#fff",
+        color: theme === "dark" ? "#fff" : "#000",
+    }}>
       <h1>Belajar React</h1>
 
       {/* Materi 1 - JSX Rules */}
@@ -40,7 +48,12 @@ const App = () => {
 
       {/* Materi 6 - Reducer */}
       <TodoList />
+
+      {/* Materi 7 - Context */}
+      <ThemeToggle />
     </div>
+
+    
   );
 };
 
---
+++ week-2/src/components/7-context/index.js
@@ -0,0 +1,8 @@
+import ThemeToggle from "./1-ThemeToggle";
+import { ThemeContext, ThemeProvider } from "./context/ThemeContext";
+
+export {
+  ThemeToggle,
+  ThemeContext,
+  ThemeProvider,
+};
